5ed0a60f71308cf02a237fce5bea9a99fcc0d4dd
Oyvindmal <oyvindmal@outlook.com>
2014-12-25 17:51:15 +0100

    Added templating and navigation

--- Script.js
+++ Script.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
-    
+    LoadPage("main");
+    var pages = [
+    { "Name": "Innkjøp", "Template": "main" },
+    { "Name": "Reisefølge", "Template": "reisefolge" },
+    { "Name": "Oppsummering", "Template": "oppsummering" }
+    ];
+
+    var defaultNav = $("#defaultnav")
+    $.each(pages, function (k, v) {
+        var elem = $("<li />");
+        var link = $("<a />");
+        link.html(v.Name);
+        link.click(function () {
+            LoadPage(v.Template);
+        })
+        link.appendTo(elem);
+        elem.appendTo(defaultNav);
+    })
 })
+
+function LoadPage(name){
+    var templateurl = "templates/" + name + ".html";
+    $("#maincontent").load(templateurl);
+}
